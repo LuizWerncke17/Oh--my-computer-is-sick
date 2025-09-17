2af9ce7c7823d94f4e809cc09d917d3a29043034
Augusto-Viana <spacek.viana@gmail.com>
2025-09-17 10:11:55 -0300

    Construção de torres

--- Computer sick game/Assets/Scripts/Health_LB1.cs
+++ Computer sick game/Assets/Scripts/Health_LB1.cs
@@ -5,11 +5,14 @@ public class Health_LB1 : MonoBehaviour
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 3;
 
+    private bool isDestroyed = false;
+
     public void takeDamage(int dmg) {
         hitPoints -= dmg;
 
-        if (hitPoints <= 0) {
+        if (hitPoints <= 0 && !isDestroyed) {
             EnemySpawner.onEnemyDestroy.Invoke();
+            isDestroyed = true;
             Destroy(gameObject);
         }
     }
